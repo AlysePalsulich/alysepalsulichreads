e403833bc9a39d2bfc7c27f93935bca49dfcbdbc
Alyse Palsulich <brpalsulich@gmail.com>
2024-01-08 13:15:09 -0700

    Initial Functioning Commit

--- apreads/client/src/App.js
+++ apreads/client/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+ 
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
- // We import all the components we need in our app
+ 
+// We import all the components we need in our app
 import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
- const App = () => {
+import TopLevelNabvbar from "./components/navbars/topLevelNavbar";
+ 
+const App = () => {
  return (
    <div>
+    <TopLevelNabvbar />
      <Navbar />
      <Routes>
        <Route exact path="/" element={<RecordList />} />
@@ -18,4 +23,5 @@ import Create from "./components/create";
    </div>
  );
 };
- export default App;
+ 
+export default App;
--- apreads/client/src/components/navbar.js
+++ apreads/client/src/components/navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
-// We import bootstrap to make our application look better.
-import "bootstrap/dist/css/bootstrap.css";
- // We import NavLink to utilize the react router.
+
+import 'bootstrap/dist/css/bootstrap.css';
+ 
+// We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
- // Here, we display our Navbar
+ 
+// Here, we display our Navbar
 export default function Navbar() {
  return (
    <div>
@@ -22,7 +24,8 @@ export default function Navbar() {
        >
          <span className="navbar-toggler-icon"></span>
        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+ 
+       <div className="collapse navbar-collapse" id="navbarSupportedContent">
          <ul className="navbar-nav ml-auto">
            <li className="nav-item">
              <NavLink className="nav-link" to="/create">
