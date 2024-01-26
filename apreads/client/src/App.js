57edfcac49e5d8b27f389df0979330d648f402f5
Alyse Palsulich <brpalsulich@gmail.com>
2024-01-25 20:10:39 -0700

    Blog update.

--- apreads/client/src/App.js
+++ apreads/client/src/App.js
@@ -9,12 +9,14 @@ import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
 import TopLevelNabvbar from "./components/navbars/topLevelNavbar";
+import Blog from "./components/blog_template/Blog";
  
 const App = () => {
  return (
    <div>
-    <TopLevelNabvbar />
+    <Blog />
      <Routes>
+       <Route exact path="/" element={<RecordList />} />
        <Route path="/edit/:id" element={<Edit />} />
        <Route path="/create" element={<Create />} />
      </Routes>
